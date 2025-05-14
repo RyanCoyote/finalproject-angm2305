d91e9d6f386e2838f8b0467b103a1ebce2ed89b6
ryanw <ryanw@filter-1.com>
2025-05-13 20:28:39 -0500

    Created classes for projectiles, projectile patterns, the player object, and main()

---
+++ src/src.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+
+class Projectile():
+
+    def __init__(self, pos=(0,0), size =20, life=1000):
+    
+
+class ProjectilePattern():
+
+
+class PlayerObject():
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
