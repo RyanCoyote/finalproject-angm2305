0806fb58a4b319f0327db9a6e55ea08ea8c5fa89
ryanw <ryanw@filter-1.com>
2025-05-13 20:34:03 -0500

    Set up main() function by initializing pygame, setting variables, running game loop, filling screen, etc.

--- src/src.py
+++ src/src.py
@@ -5,6 +5,12 @@ import pygame
 class Projectile():
 
     def __init__(self, pos=(0,0), size =20, life=1000):
+
+    def update(self, dt):
+
+    def update_surface(self):
+    
+    def draw(self, surface):
     
 
 class ProjectilePattern():
@@ -14,6 +20,25 @@ class PlayerObject():
 
 
 def main():
+    pygame.init()
+    pygame.display.set_caption("Bullet Hell")
+    clock = pygame.time.Clock()
+    dt = 0
+    resolution = (1920, 1080)
+    screen = pygame.display.set_mode(resolution)
+    running = True
+    while running:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                     running = False
+
+        # Add update function here later for projectiles
+
+        screen.fill('Black')
+        # Call on draw function here later
+        pygame.display.flip()
+        dt = clock.tick(64)
+    pygame.quit()
 
 
 if __name__ == "__main__":
